535b72b7d3d5d3f145ab71e722c3b04b17e4fda1
RaniPatel16 <rani.r.patel.cg@gmail.com>
2026-04-18 01:49:16 +0530

    add create note endpoint (POST /api/notes)

--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const {
-   
+     createNote
 } = require('../controllers/note.controller');
 
 // CRUD routes
-
+router.post('/', createNote)
 module.exports = router;
